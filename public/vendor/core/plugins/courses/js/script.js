b4aada18c82ffbd24698d4fab720cafd34bfbf5c
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-10-24 15:23:05 +0200

    Update script.js

--- public/vendor/core/plugins/courses/js/script.js
+++ public/vendor/core/plugins/courses/js/script.js
@@ -1,1 +1,87 @@
-$(function(){function n(n){var o=$(n.data("target")),t=n.attr("name");"unlimited_seats"===t&&(n.is(":checked")?o.hide().find("input").val(""):o.show()),"is_recurring"===t&&(n.is(":checked")?o.show():o.hide().find("input, select").val(""))}$('[data-toggle="toggle-field"]').each(function(){var o=$(this);n(o),o.on("change",function(){n(o)})}),$(document).on("click",".view-participants-btn",function(){var n=$(this).data("session-id"),o=$("#participantsModalContent");o.html('<i class="fa fa-spinner fa-spin"></i> Loading...');var t="/admin/course-sessions/".concat(n,"/bookings");$.ajax({url:t,method:"GET",success:function(n){o.html(n)},error:function(){o.html("<p>Error loading participants.</p>")}}),$("#participantsModal").modal("show")});var o=$("#admin_course_id"),t=$("#admin_session_id"),i=t.data("selected");o.on("change",function(){var n=$(this).val();if(n){var o="/admin/courses/".concat(n,"/sessions");$.ajax({url:o,type:"GET",beforeSend:function(){t.html("<option>Loading sessions...</option>")},success:function(n){if(n.data&&n.data.length){var o='<option value="">Select Session</option>';n.data.forEach(function(n){var t=n.id==i?"selected":"";o+='<option value="'.concat(n.id,'" ').concat(t,">").concat(n.text,"</option>")}),t.html(o)}else t.html('<option value="">No sessions found</option>')},error:function(){t.html('<option value="">Error loading sessions</option>')}})}else t.html('<option value="">Select a course first</option>')}),o.val()&&o.trigger("change")});
+/**/ (() => { // webpackBootstrap
+/!*******************!\
+  !* ./platform/plugins/courses/resources/js/script.js *!
+  \*******************/
+$(function () {
+  function toggleField($checkbox) {
+    var target = $($checkbox.data('target'));
+    var field = $checkbox.attr('name');
+    if (field === 'unlimited_seats') {
+      if ($checkbox.is(':checked')) {
+        target.hide().find('input').val('');
+      } else {
+        target.show();
+      }
+    }
+    if (field === 'is_recurring') {
+      if ($checkbox.is(':checked')) {
+        target.show();
+      } else {
+        target.hide().find('input, select').val('');
+      }
+    }
+  }
+  $('[data-toggle="toggle-field"]').each(function () {
+    var $checkbox = $(this);
+    toggleField($checkbox);
+    $checkbox.on('change', function () {
+      toggleField($checkbox);
+    });
+  });
+  $(document).on('click', '.view-participants-btn', function () {
+    var sessionId = $(this).data('session-id');
+    var modalContent = $('#participantsModalContent');
+    modalContent.html('<i class="fa fa-spinner fa-spin"></i> Loading...');
+    var url = "/admin/course-sessions/".concat(sessionId, "/bookings");
+    $.ajax({
+      url: url,
+      method: 'GET',
+      success: function success(html) {
+        modalContent.html(html);
+      },
+      error: function error() {
+        modalContent.html('<p>Error loading participants.</p>');
+      }
+    });
+    $('#participantsModal').modal('show');
+  });
+  var $courseSelect = $('#admin_course_id');
+  var $sessionSelect = $('#admin_session_id');
+  var selectedSessionId = $sessionSelect.data('selected');
+  $courseSelect.on('change', function () {
+    var courseId = $(this).val();
+    if (!courseId) {
+      $sessionSelect.html('<option value="">Select a course first</option>');
+      return;
+    }
+    var url = "/admin/courses/".concat(courseId, "/sessions");
+    $.ajax({
+      url: url,
+      type: 'GET',
+      beforeSend: function beforeSend() {
+        $sessionSelect.html('<option>Loading sessions...</option>');
+      },
+      success: function success(response) {
+        if (response.data && response.data.length) {
+          var options = '<option value="">Select Session</option>';
+          response.data.forEach(function (session) {
+            var selectedAttr = session.id == selectedSessionId ? 'selected' : '';
+            options += "<option value=\"".concat(session.id, "\" ").concat(selectedAttr, ">").concat(session.text, "</option>");
+          });
+          $sessionSelect.html(options);
+        } else {
+          $sessionSelect.html('<option value="">No sessions found</option>');
+        }
+      },
+      error: function error() {
+        $sessionSelect.html('<option value="">Error loading sessions</option>');
+      }
+    });
+  });
+  var initialCourseId = $courseSelect.val();
+  if (initialCourseId) {
+    $courseSelect.trigger('change');
+  }
+});
+/**/ })()
+;
