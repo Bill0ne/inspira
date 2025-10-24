a1349a6de832f32c6d4666eb4665844769f8e344
Zohaib Akhtar <147222210+zaibiiu@users.noreply.github.com>
2025-10-24 16:34:09 +0500

    fixes

---
+++ storage/framework/views/96894b9e6e80a49770b2b4fa0dffac7a.php
@@ -0,0 +1,16 @@
+<svg class="icon icon-sm svg-icon-ti-ti-external-link"
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  >
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+  <path d="M11 13l9 -9" />
+  <path d="M15 4h5v5" />
+</svg><?php /**PATH D:\xampp\htdocs\inspira\storage\framework\views/734a85ad450bff1f1529ad0b833e8b61.blade.php ENDPATH**/ ?>
