bbbff6510c3bd6385b2293e2d89d640564bbe7bc
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-10-28 08:14:00 +0100

    Refine course analytics table presentation

--- platform/plugins/courses/config/courses.php
+++ platform/plugins/courses/config/courses.php
@@ -2,4 +2,9 @@
 
 return [
     'default_number_start_number' => env('COURSE_DEFAULT_NUMBER_START_NUMBER', 1000000),
+
+    'performance' => [
+        'max_views_reference' => env('COURSE_MAX_VIEWS_REFERENCE', 500),
+        'analytics_period_days' => env('COURSE_ANALYTICS_PERIOD_DAYS', 30),
+    ],
 ];
