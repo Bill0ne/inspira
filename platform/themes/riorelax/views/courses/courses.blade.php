e2377c3efe6e35f7b3f9f5e419095ae202fa4ef9
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-10-29 18:36:10 +0100

    Update courses.blade.php

--- platform/themes/riorelax/views/courses/courses.blade.php
+++ platform/themes/riorelax/views/courses/courses.blade.php
@@ -1,11 +1,7 @@
 @php(Theme::set('pageTitle', 'Kurse'))
-{{-- Inspira: Kursübersichtsseite – ohne Sidebar, volle Breite (12 Columns) --}}
-{{-- Dateipfad: /platform/themes/inspira/views/courses.blade.php --}}
 
 <section class="container courses-page my-5">
-    {{-- Optional: Filterbereich (kommt später) --}}
-    {{-- @include(Theme::getThemeNamespace('partials.filters')) --}}
-
+    @include(Theme::getThemeNamespace('partials.filters'))
     <div class="row">
         <div class="col-lg-12">
             {{-- Kursübersicht --}}
