f4f769569d0ee4f427c34d3a0a00f43f2e385336
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-10-29 18:28:38 +0100

    Update index.blade.php

--- platform/themes/riorelax/partials/shortcodes/all-rooms/index.blade.php
+++ platform/themes/riorelax/partials/shortcodes/all-rooms/index.blade.php
@@ -1,19 +1,19 @@
 <section class="services-area pt-20 pb-40">
-    <h3 class="mb-20">{{ __(':count rooms available', ['count' => $rooms->total()]) }}</h3>
-
     @if ($rooms->isNotEmpty())
-        <div class="row">
+        <div class="row g-4">
             @foreach ($rooms as $room)
-                <div class="col-md-6">
+                <div class="col-lg-4 col-md-6 col-sm-12">
                     {!! Theme::partial('rooms.item', compact('room', 'startDate', 'endDate', 'nights', 'adults')) !!}
                 </div>
             @endforeach
         </div>
+
         @if ($rooms instanceof \Illuminate\Contracts\Pagination\LengthAwarePaginator)
             <div class="text-center mt-30">
                 {!! $rooms->withQueryString()->links(Theme::getThemeNamespace('partials.pagination')) !!}
             </div>
         @endif
+    @else
+        <p>{{ __('Derzeit sind keine Räume verfügbar') }}</p>
     @endif
 </section>
-
