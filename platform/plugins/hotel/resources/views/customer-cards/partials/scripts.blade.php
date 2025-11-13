34e9326318a8da464d94fea4be7e64dd086bfe66
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-11-13 12:59:15 +0100

    Restrict course checkout assets

--- platform/plugins/hotel/resources/views/customer-cards/partials/scripts.blade.php
+++ platform/plugins/hotel/resources/views/customer-cards/partials/scripts.blade.php
@@ -1,27 +1,35 @@
-@push('header')
-    <script>
-        'use strict';
+@php
+    $shouldRegisterCustomerCardAssets = is_in_admin() || \Illuminate\Support\Facades\Route::is('public.course.*');
+@endphp
 
-        window.customerCard = window.customerCard || {};
-        window.customerCard.currency = '{{ get_application_currency()->symbol }}';
-        window.customerCard.routes = {
-            apply: '{{ route('ajax.customer-card.apply') }}',
-            remove: '{{ route('ajax.customer-card.remove') }}',
-        };
-        window.trans = window.trans || {};
-        window.trans.customerCard = {{ Js::from(trans('plugins/hotel::customer-card')) }};
-    </script>
-@endpush
-
-@push('footer')
+@if (! $shouldRegisterCustomerCardAssets)
     {!! $jsValidator ?? '' !!}
+@else
+    @push('header')
+        <script>
+            'use strict';
 
-    @if (! is_in_admin())
-        @php
-            Theme::asset()
-                ->container('footer')
-                ->add('payment-http-client', 'vendor/core/plugins/payment/js/http-client.js', ['jquery'])
-                ->add('hotel-customer-card-js', 'vendor/core/plugins/hotel/js/customer-card.js', ['payment-http-client']);
-        @endphp
-    @endif
-@endpush
+            window.customerCard = window.customerCard || {};
+            window.customerCard.currency = '{{ get_application_currency()->symbol }}';
+            window.customerCard.routes = {
+                apply: '{{ route('ajax.customer-card.apply') }}',
+                remove: '{{ route('ajax.customer-card.remove') }}',
+            };
+            window.trans = window.trans || {};
+            window.trans.customerCard = {{ Js::from(trans('plugins/hotel::customer-card')) }};
+        </script>
+    @endpush
+
+    @push('footer')
+        {!! $jsValidator ?? '' !!}
+
+        @if (! is_in_admin())
+            @php
+                Theme::asset()
+                    ->container('footer')
+                    ->add('payment-http-client', 'vendor/core/plugins/payment/js/http-client.js', ['jquery'])
+                    ->add('hotel-customer-card-js', 'vendor/core/plugins/hotel/js/customer-card.js', ['payment-http-client']);
+            @endphp
+        @endif
+    @endpush
+@endif
