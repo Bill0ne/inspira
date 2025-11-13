94179b44ddb6eafa88513c1873490720a1cbe7bb
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-11-13 09:10:49 +0100

    Fix footer filter constant for customer card config

---
+++ platform/plugins/hotel/resources/views/customer-cards/partials/config.blade.php
@@ -0,0 +1,9 @@
+<script>
+    window.customerCard = {
+        currency: "{{ get_application_currency()->symbol }}",
+        routes: {
+            apply: "{{ route('public.customer-card.apply') }}",
+            remove: "{{ route('public.customer-card.remove') }}"
+        }
+    };
+</script>
--- platform/plugins/hotel/src/Providers/HotelServiceProvider.php
+++ platform/plugins/hotel/src/Providers/HotelServiceProvider.php
@@ -471,5 +471,9 @@ class HotelServiceProvider extends ServiceProvider
             Captcha::registerFormSupport(ForgotPasswordForm::class, ForgotPasswordRequest::class, trans('plugins/hotel::hotel.forgot_password_form'));
             Captcha::registerFormSupport(ResetPasswordForm::class, ResetPasswordRequest::class, trans('plugins/hotel::hotel.reset_password_form'));
         }
+
+        add_filter(THEME_FRONT_FOOTER, function (?string $html): string {
+            return ($html ?? '') . view('plugins/hotel::customer-cards.partials.config')->render();
+        });
     }
 }
