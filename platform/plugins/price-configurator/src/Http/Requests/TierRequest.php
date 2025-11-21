1626fb2aa389c72bf7fde79d4f932dcf86dba05b
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-11-21 14:30:00 +0100

    Handle localized date inputs for price tiers

--- platform/plugins/price-configurator/src/Http/Requests/TierRequest.php
+++ platform/plugins/price-configurator/src/Http/Requests/TierRequest.php
@@ -5,10 +5,51 @@ namespace Botble\PriceConfigurator\Http\Requests;
 use Botble\Base\Rules\OnOffRule;
 use Botble\PriceConfigurator\Enums\PriceConfiguratorStatusEnum;
 use Botble\Support\Http\Requests\Request;
+use Carbon\Carbon;
 use Illuminate\Validation\Rule;
 
 class TierRequest extends Request
 {
+    protected function prepareForValidation(): void
+    {
+        $formattedDates = [];
+
+        foreach (['starts_at', 'ends_at'] as $field) {
+            $value = $this->input($field);
+
+            if (blank($value)) {
+                continue;
+            }
+
+            $parsed = $this->parseDate($value);
+
+            if ($parsed) {
+                $formattedDates[$field] = $parsed->format('Y-m-d H:i:s');
+            }
+        }
+
+        if ($formattedDates !== []) {
+            $this->merge($formattedDates);
+        }
+    }
+
+    protected function parseDate(string $value): ?Carbon
+    {
+        foreach (['Y-m-d H:i:s', 'Y-m-d H:i', 'd.m.Y H:i', 'd.m.Y H:i:s'] as $format) {
+            $parsed = Carbon::createFromFormat($format, $value);
+
+            if ($parsed !== false) {
+                return $parsed;
+            }
+        }
+
+        try {
+            return Carbon::parse($value);
+        } catch (\Throwable) {
+            return null;
+        }
+    }
+
     public function rules(): array
     {
         return [
