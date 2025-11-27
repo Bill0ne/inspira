a3dadd097332e1e52c886c5be46bd1fa56d8d115
Bill0ne <143216302+Bill0ne@users.noreply.github.com>
2025-11-27 12:49:07 +0100

    Revert "Ensure customer card usage is finalized after processing"

--- platform/plugins/courses/src/Providers/HookServiceProvider.php
+++ platform/plugins/courses/src/Providers/HookServiceProvider.php
@@ -74,25 +74,20 @@ class HookServiceProvider extends ServiceProvider
                 if ($payment) {
                     $paymentMethod = $bookingService->normalizePaymentChannel($payment->payment_channel);
 
-                    if ($booking->customer_card_id) {
-                        $paymentMethod = $bookingService->normalizePaymentChannel(PaymentMethodEnum::CUSTOMER_CARD());
-                    }
-
                     $booking->forceFill([
                         'payment_id' => $payment->getKey(),
                         'payment_method' => $paymentMethod,
                     ]);
 
-                    if ($payment->status === PaymentStatusEnum::COMPLETED || $booking->customer_card_id) {
+                    if ($payment->status === PaymentStatusEnum::COMPLETED) {
                         $booking->status = BookingStatusEnum::PROCESSING;
                     }
                 }
 
-                if ($booking->customer_card_id) {
+                if ($booking->customer_card_id && ! $booking->payment_method) {
                     $booking->payment_method = $bookingService->normalizePaymentChannel(
                         PaymentMethodEnum::CUSTOMER_CARD()
                     );
-                    $booking->status = BookingStatusEnum::PROCESSING;
                 }
 
                 if ($booking->isDirty()) {
